956e49611baeb08b2dcfba6931fa73e47890ca49
Ahmad Nawab <ahmad.nawab@ecmwf.int>
2023-07-05 10:21:10 +0000

    Added tests for gpu=deepcopy flag

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ This repository contains a series of unit-tests to test standard compliance of c
 
 
 Building
-========
+--------
 Building the unit-test suite is as simple as:
 ```
 cmake -B build
@@ -13,7 +13,7 @@ cmake --build build
 ```
 
 Running test-suite
-==================
+------------------
 For both OpenACC and OpenMP, there are two categories of tests:
  - 'arguments': Data is passed down to device-code as subroutine arguments
  - 'globalvars': Data is imported into device-code as module variables
@@ -21,4 +21,11 @@ For both OpenACC and OpenMP, there are two categories of tests:
 The tests can therefore be filtered accordingly:
 ```
 ctest --test-dir build [-R <openacc/openmp> <arg/globalvar>]
+```
+
+OpenACC deepcopy
+----------------
+This suite also contains tests to verify the functionality of the OpenACC `-gpu=deepcopy` flag. These tests can be enabled at build-time:
+```
+cmake -B build -DDEEPCOPY=ON
 ```
---
+++ openacc/globalvars/derived_type_nested_allocatable_deepcopy.F90
@@ -0,0 +1,82 @@
+module vars_mod
+  type :: container
+     integer, allocatable :: x(:)
+  end type container
+
+  type :: bucket
+     type(container), allocatable :: a(:), b(:), c(:)
+  end type bucket
+
+  type(bucket) :: v
+!$acc declare create(v)
+end module vars_mod
+
+module kernel_mod
+contains
+subroutine kernel(j,n)
+use vars_mod, only: v
+implicit none
+
+integer, intent(in) :: j,n
+integer :: i
+!$acc routine vector
+!$acc data present(v)
+
+!$acc loop vector
+do i=1,n
+   v%c(j)%x(i) = v%a(j)%x(i) + v%b(j)%x(i)
+enddo
+
+!$acc end data
+end subroutine kernel
+end module kernel_mod
+
+program main
+use vars_mod, only: v
+use kernel_mod, only: kernel
+implicit none
+integer :: i,j,n,m
+
+n = 32
+m = 128
+
+allocate(v%a(m))
+allocate(v%b(m))
+allocate(v%c(m))
+do j=1,m
+   allocate(v%a(j)%x(n))
+   allocate(v%b(j)%x(n))
+   allocate(v%c(j)%x(n))
+   v%c(j)%x = 0
+   do i=1,n
+     v%a(j)%x(i) = i
+     v%b(j)%x(i) = 2*v%a(j)%x(i)
+   enddo
+enddo
+
+!$acc update device(v)
+
+!$acc parallel loop gang
+do j=1,m
+  call kernel(j,n)
+enddo
+
+!$acc update self(v)
+
+do j=1,m
+  do i=1,n
+    if(v%c(j)%x(i) /= (v%a(j)%x(i) + v%b(j)%x(i)))then
+       print *, i, v%c(j)%x(i), v%a(j)%x(i), v%b(j)%x(i)
+       error stop
+    endif
+  enddo
+
+  deallocate(v%a(j)%x)
+  deallocate(v%b(j)%x)
+  deallocate(v%c(j)%x)
+enddo
+deallocate(v%a)
+deallocate(v%b)
+deallocate(v%c)
+
+end program main
